ddc33ac6c2397966b1c0cf3328cd16e3ba7f37eb
ketan modi <ketanmodi@kanhasofttech.com>
2023-08-22 19:16:14 +0530

    Auth apis

--- modules/auth/controllers/auth.server.controllers.js
+++ modules/auth/controllers/auth.server.controllers.js
@@ -1,9 +1,11 @@
-const sendResponse = require("../../helpers/common");
 const bcrypt = require("bcrypt");
+var jwt = require('jsonwebtoken');
+const sendResponse = require("../../helpers/common");
 const UserModel = require("../models/auth.server.models");
+var privateKey = "TestPrivateKey";
 
 const SignUp = async (req = null) => {
-    if (req?.body) {
+    if (req?.body?.email) {
         let exist = await UserModel.find({ email: req?.body?.email });
         if (exist.length) {
             return sendResponse(false, 400, {}, "Email already exist, Please try with another email or login with the existing email address!");
@@ -32,8 +34,37 @@ const SignUp = async (req = null) => {
             return sendResponse(false, 400, {}, transformedObject);
         }
     }
+    return sendResponse(false, 400, {}, "Something went wrong, Please try again with your details!");
+}
+
+const SignIn = async (req = null) => {
+    if (req?.body?.email && req?.body?.password) {
+        let user = await UserModel.find({ email: req?.body?.email });
+        if (!user.length) {
+            return sendResponse(false, 400, {}, "Email not registered!");
+        }
+        let hashed = await bcrypt.compare(req?.body?.password, user[0]?.password);
+        if(!hashed){
+            return sendResponse(false, 400, {}, "Invalid password!");
+        }
+        else{
+            var token = jwt.sign({
+                name: user[0]?.name,
+                email: user[0]?.email,
+            }, privateKey, );
+            let withoutPassword = user[0].toObject();
+            delete withoutPassword.password;
+            let loggedInUser = {
+                user: withoutPassword,
+                token: token,
+            }
+            return sendResponse(true, 200, loggedInUser, "Loggedin successfully!");
+        }
+    }
+    return sendResponse(false, 400, {}, "Something went wrong, Please try again with correct credentials!");
 }
 
 module.exports = {
     SignUp,
+    SignIn
 }
--- modules/auth/routes/auth.routes.server.js
+++ modules/auth/routes/auth.routes.server.js
@@ -5,6 +5,11 @@ const Router = express.Router();
 Router.post('/signup', async (req, resp)=>{
     let user = await AuthController.SignUp(req);
     resp.status(user?.code).json(user);
+});
+
+Router.post('/signin', async (req, resp)=>{
+    let user = await AuthController.SignIn(req);
+    resp.status(user?.code).json(user);
 })
 
 module.exports = Router;
